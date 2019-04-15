bb64bee66317bcb905ff3cfc5e6fde0366d961f9
KrKrainov <k.a.krainov@gmail.com>
2019-04-15 16:47:30 +0300

    refactoring ItemStatusFilter component

--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -1,14 +1,31 @@
 import React, { PureComponent } from "react";
 
 export default class ItemStatusFilter extends PureComponent {
+    buttons = [
+        { name: 'all', label: 'All' },
+        { name: 'important', label: 'Important' },
+        { name: 'done', label: 'Done' }
+    ];
+
     render() {
         const { toggleFilter, filter } = this.props;
 
+        const buttons = this.buttons.map(({ name, label }) => {
+            const activeClass = filter === name ? 'btn-info' : 'btn-outline-secondary';
+            return (
+                <button
+                    key={name}
+                    type="button"
+                    className={`btn ${activeClass}`}
+                    onClick={() => toggleFilter(name)}>
+                    {label}
+                    </button>
+            );
+        });
+
         return (
             <div className="btn-group float-right mb-3">
-                <button type="button" className={`btn ${filter === 'all' ? 'btn-info' : 'btn-outline-secondary'}`} onClick={() => toggleFilter('all')}>All</button>
-                <button type="button" className={`btn ${filter === 'important' ? 'btn-info' : 'btn-outline-secondary'}`} onClick={() => toggleFilter('important')}>Important</button>
-                <button type="button" className={`btn ${filter === 'done' ? 'btn-info' : 'btn-outline-secondary'}`} onClick={() => toggleFilter('done')}>Done</button>
+                {buttons}
             </div>
         )
     }
